c612108fda714ae1783161bbe252abc741370951
Reto Weiss <reto.weiss@ivyteam.ch>
2017-12-24 14:56:33 +0100

    * Add support for utf-8 encoded console output

--- src/main/java/ch/weiss/terminal/windows/AnsiTerminalForWindows.java
+++ src/main/java/ch/weiss/terminal/windows/AnsiTerminalForWindows.java
@@ -1,5 +1,12 @@
 package ch.weiss.terminal.windows;
 
+import java.io.BufferedOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
 import com.sun.jna.Platform;
 import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.WinBase;
@@ -9,6 +16,7 @@ import com.sun.jna.ptr.IntByReference;
 
 public class AnsiTerminalForWindows
 {
+  private static final int UTF_8_CODE_PAGE = 65001;
   private static final int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
 
   /**
@@ -20,10 +28,7 @@ public class AnsiTerminalForWindows
    */
   public static boolean enableVirtualTerminalProcessing()
   {
-    if (!Platform.isWindows())
-    {
-      return false;
-    }
+    checkWindows();
 
     Wincon windowsConsoleApi = Kernel32.INSTANCE;
     HANDLE hConsole = windowsConsoleApi.GetStdHandle(Wincon.STD_OUTPUT_HANDLE);
@@ -36,15 +41,58 @@ public class AnsiTerminalForWindows
     boolean result = windowsConsoleApi.GetConsoleMode(hConsole, dwModeRef);
     if (!result)
     {
-      return result;
+      return false;
     }
 
     int dwMode = dwModeRef.getValue() + ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     result = windowsConsoleApi.SetConsoleMode(hConsole, dwMode);
     if (!result)
     {
-      return result;
+      return false;
     }
     return true;
+  }
+
+  public static boolean enableUtf8CodePage()
+  {
+    checkWindows();
+
+    Wincon windowsConsoleApi = Kernel32.INSTANCE;
+    boolean result = windowsConsoleApi.SetConsoleOutputCP(UTF_8_CODE_PAGE);
+    if (!result)
+    {
+      return false;
+    }
+    try
+    {
+      @SuppressWarnings("resource")
+      PrintStream out = getUtf8EncodedPrintStream(FileDescriptor.out);
+      System.setOut(out);
+      @SuppressWarnings("resource")
+      PrintStream err = getUtf8EncodedPrintStream(FileDescriptor.err);
+      System.setOut(err);
+    }
+    catch (@SuppressWarnings("unused") UnsupportedEncodingException ex)
+    {
+      return false;
+    }
+    return true;
+  }
+  
+  private static PrintStream getUtf8EncodedPrintStream(FileDescriptor file) throws UnsupportedEncodingException
+  {
+    FileOutputStream fos = new FileOutputStream(file);
+    BufferedOutputStream buffered = new BufferedOutputStream(fos, 128);
+    String encoding = StandardCharsets.UTF_8.name();
+    PrintStream ps = new PrintStream(buffered, true, encoding);
+    return ps;
+  }
+  
+  private static void checkWindows()
+  {
+    if (!Platform.isWindows())
+    {
+      throw new UnsupportedOperationException("AnsiTerminalForWindows is not supported on "+System.getProperty("os.name"));
+    }
   }
 }
