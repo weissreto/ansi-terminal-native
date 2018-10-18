be8a88f496c140ef4fd3969c679a17e34dcf20b1
Reto Weiss <reto.weiss@ivyteam.ch>
2018-10-18 22:18:47 +0200

    Initial support for java module system
    Upgrade to jna 5.0.0

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-SET JAVA_HOME=c:\program files\java\jdk-9.0.1
+SET JAVA_HOME=c:\program files\java\jdk-11
 SET MAVEN=C:\Tools\maven\apache-maven-3.5.2\bin\mvn
 
 %MAVEN% exec:java -Dexec.mainClass=ch.rweiss.terminal.windows.ManualTestAnsiTerminalForWindows -Dexec.arguments="%*" -Dexec.classpathScope=test
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ch.rweiss.terminal.nativ
+{
+  exports ch.rweiss.terminal.windows;
+  exports ch.rweiss.terminal.linux;
+  
+  requires com.sun.jna;
+  requires com.sun.jna.platform;
+}
