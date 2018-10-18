d9caa250485cd6b9328c92fc9bc98a839b6a0bb4
Reto Weiss <reto.weiss@ivyteam.ch>
2018-10-18 22:40:49 +0200

    Provide jna transitive because it is used in AnsiTerminal class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module ch.rweiss.terminal.nativ
   exports ch.rweiss.terminal.windows;
   exports ch.rweiss.terminal.linux;
   
-  requires com.sun.jna;
+  requires transitive com.sun.jna;
   requires com.sun.jna.platform;
 }
