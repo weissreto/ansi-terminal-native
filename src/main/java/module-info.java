440b94b55ead11ee90a72bdbde0e241ef8ec67d3
Reto Weiss <reto.weiss@ivyteam.ch>
2018-10-20 16:01:41 +0200

    Export bitset package because it is used outside the module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ch.rweiss.terminal.nativ
 {
   exports ch.rweiss.terminal.windows;
   exports ch.rweiss.terminal.linux;
+  exports ch.rweiss.bitset;
   
   requires transitive com.sun.jna;
   requires com.sun.jna.platform;
