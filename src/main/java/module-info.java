1b4642730867a6c31ac4c2f997851d5f5632099f
Reto Weiss <reto.weiss@ivyteam.ch>
2018-11-19 21:50:36 +0100

    * com.sun.jna is not transitive required

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,6 @@ module ch.rweiss.terminal.nativ
   exports ch.rweiss.terminal.nativ.linux;
   exports ch.rweiss.bitset;
   
-  requires transitive com.sun.jna;
+  requires com.sun.jna;
   requires com.sun.jna.platform;
 }
